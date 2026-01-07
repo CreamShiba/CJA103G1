987720d6be751cdf78477fb0c44c954983e11781
Eagan <eagle890915.gmail.com>
2026-01-07 22:19:01 +0800

    update model & controller of productimage; add vo for product and rating

---
+++ src/main/java/com/karshop/rating/model/RatingVO.java
@@ -0,0 +1,89 @@
+package com.karshop.rating.model;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "rating")
+public class RatingVO {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "rating_no")
+    private Integer ratingNo;
+
+    @Column(name = "ord_no")
+    private Integer ordNo;
+
+    @Column(name = "seller_no")
+    private Integer sellerNo;
+
+    @Column(name = "member_no")
+    private Integer memberNo;
+
+    @Column(name = "rating_score")
+    private Integer ratingScore;
+
+    @Column(name = "rating_comment")
+    private String ratingComment;
+
+    @Column(name = "rating_date")
+    private LocalDateTime ratingDate;
+
+    public Integer getRatingNo() {
+        return ratingNo;
+    }
+
+    public void setRatingNo(Integer ratingNo) {
+        this.ratingNo = ratingNo;
+    }
+
+    public Integer getOrdNo() {
+        return ordNo;
+    }
+
+    public void setOrdNo(Integer ordNo) {
+        this.ordNo = ordNo;
+    }
+
+    public Integer getSellerNo() {
+        return sellerNo;
+    }
+
+    public void setSellerNo(Integer sellerNo) {
+        this.sellerNo = sellerNo;
+    }
+
+    public Integer getMemberNo() {
+        return memberNo;
+    }
+
+    public void setMemberNo(Integer memberNo) {
+        this.memberNo = memberNo;
+    }
+
+    public Integer getRatingScore() {
+        return ratingScore;
+    }
+
+    public void setRatingScore(Integer ratingScore) {
+        this.ratingScore = ratingScore;
+    }
+
+    public String getRatingComment() {
+        return ratingComment;
+    }
+
+    public void setRatingComment(String ratingComment) {
+        this.ratingComment = ratingComment;
+    }
+
+    public LocalDateTime getRatingDate() {
+        return ratingDate;
+    }
+
+    public void setRatingDate(LocalDateTime ratingDate) {
+        this.ratingDate = ratingDate;
+    }
+}
