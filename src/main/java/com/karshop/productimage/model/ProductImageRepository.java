850269910205013d457a75afb6bd93ded9a27b66
Eagan <eagle890915.gmail.com>
2026-01-08 20:28:29 +0800

    add repository for all

---
+++ src/main/java/com/karshop/productimage/model/ProductImageRepository.java
@@ -0,0 +1,6 @@
+package com.karshop.productimage.model;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductImageRepository extends JpaRepository<ProductImageVO, Integer> {
+}
