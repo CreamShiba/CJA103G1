169c43bbaf6445162a58cecadd160c7ec7dde0e6
Eagan <eagle890915.gmail.com>
2026-01-08 20:28:29 +0800

    add repository for all

---
+++ src/main/java/com/karshop/ord/model/OrdRepository.java
@@ -0,0 +1,6 @@
+package com.karshop.ord.model;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface OrdRepository extends JpaRepository<OrdVO, Integer> {
+}
---
+++ src/main/java/com/karshop/orddetail/model/OrdDetailRepository.java
@@ -0,0 +1,6 @@
+package com.karshop.orddetail.model;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface OrdDetailRepository extends JpaRepository<OrdDetailVO, Integer> {
+}
