600f873d49810c56dbae826a427077ea5713b2c3
Eagan <eagle890915.gmail.com>
2026-01-08 20:28:29 +0800

    add repository for all

--- src/main/java/com/karshop/rating/model/RatingRepository.java
+++ src/main/java/com/karshop/rating/model/RatingRepository.java
@@ -1,16 +1,6 @@
 package com.karshop.rating.model;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
-
-import java.util.List;
 
 public interface RatingRepository extends JpaRepository<RatingVO, Integer> {
-
-    @Query("SELECT r.ord.ordNo FROM RatingVO r WHERE r.seller.sellerNo = :sellerNo")
-    List<Integer> findRatedOrderNoBySeller(@Param("sellerNo") Integer sellerNo);
-
-    List<RatingVO> findBySellerSellerNo(Integer sellerNo);
-
 }
