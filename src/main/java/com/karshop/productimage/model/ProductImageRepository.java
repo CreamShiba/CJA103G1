238b925702612055928c228bb151a635c7e8d46f
Eagan <eagle890915.gmail.com>
2026-01-10 17:54:05 +0800

    test display image for seller

--- src/main/java/com/karshop/productimage/model/ProductImageRepository.java
+++ src/main/java/com/karshop/productimage/model/ProductImageRepository.java
@@ -2,5 +2,8 @@ package com.karshop.productimage.model;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+
 public interface ProductImageRepository extends JpaRepository<ProductImageVO, Integer> {
+
+
 }
