98af9ad29807b22bc1986737a6e3117cb14b3a80
Eagan <eagle890915.gmail.com>
2026-01-20 22:07:11 +0800

    modify package name

--- src/main/java/com/karshop/productcategory/model/ProductCategoryRepository.java
+++
@@ -1,7 +0,0 @@
-package com.karshop.productcategory.model;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProductCategoryRepository extends JpaRepository<ProductCategoryVO, Integer> {
-    // 內建 findAll() 就夠用了
-}
