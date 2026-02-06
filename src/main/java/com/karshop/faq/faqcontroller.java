729ab01235bb931a0eecef2481248842dd02e36f
wzw <wzw950517@gmail.com>
2026-02-06 23:04:53 +0800

    update sellerinfo+faq+html

--- src/main/java/com/karshop/faq/faqcontroller.java
+++ src/main/java/com/karshop/faq/faqcontroller.java
@@ -112,7 +112,7 @@ public class faqcontroller {
         faq entity = service.getFaqById(id);
         if (entity != null) {
             // 切換狀態：發布 → 草稿，草稿 → 發布
-            String newStatus = entity.getStatus().equals("發佈") ? "草稿" : "發佈";
+            String newStatus = entity.getStatus().equals("已發佈") ? "草稿" : "已發佈";
             entity.setStatus(newStatus);
             service.updateFaq(id, entity);
         }
